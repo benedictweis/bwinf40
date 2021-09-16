26609c44348dd306ca20c3fef6ae045ba34fe101
Benegamz <benegamz@t-online.de>
2021-09-16 09:02:43 +0200

    Initital commit a2

---
+++ a2/app/src/main/java/a2/Hotel.java
@@ -0,0 +1,12 @@
+package a2;
+
+public class Hotel{
+    public int id;
+    public int distance;
+    public float rating;
+
+    public Hotel (int id,int distance,float rating){
+        Hotel h = new Hotel;
+
+    }
+}
