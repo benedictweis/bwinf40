1a69696e1286d0a2565b1a9d2f239029cb364c61
tMB04 <90890694+tMB04@users.noreply.github.com>
2021-09-28 12:23:04 +0200

    found solution

--- a2/app/src/main/java/a2/App.java
+++ a2/app/src/main/java/a2/App.java
@@ -57,71 +57,17 @@ public class App {
             totalTime = 1800;
         }
 
-        int currentTravelTime = 0;
         float averageRating;
         ArrayList<Hotel> selectedHotels = new ArrayList<Hotel>();
-        Boolean usingAlternateMethod = false;
-        // -360 because we can reach end from there
-        while (currentTravelTime < totalTime - 360) {
-            // try to make it to the end using only the best Hotels within Range
-            Hotel currentHotel = getBestHotelWithinRange(hotels, currentTravelTime);
-            selectedHotels.add(currentHotel);
-            currentTravelTime = currentHotel.distance;
-            // if this fails use the other method
-            if (selectedHotels.size() > 5) {
 
-                usingAlternateMethod = true;
-                break;
-            }
-        }
-        usingAlternateMethod = true;
-        if (usingAlternateMethod) {
-            // clear values from previous attempt
-            selectedHotels = createRoute(totalTime, hotels);
-            
-            for (Hotel h : selectedHotels)
-                System.out.println(h.distance + " " + h.rating);
-            System.out.println("------");
-            int previousHotelDistance = 0;
-            int nextHotelDistance = 0;
-            for (int i = selectedHotels.size() - 1; i > -1; i--) {
-                // adjust the hotels to try and optimize the average rating
-                if (i == 0) {
-                    previousHotelDistance = 0;
-                } else {
-                    previousHotelDistance = selectedHotels.get(i - 1).distance;
-                }
-                if (i == selectedHotels.size() - 1) {
-                    nextHotelDistance = totalTime;
-                } else {
-                    nextHotelDistance = selectedHotels.get(i + 1).distance;
-                }
+        selectedHotels = createRoute(totalTime, hotels);
 
-                Hotel currentHotel = selectedHotels.get(i);
-                for (Hotel h : getRemainingHotels(hotels, selectedHotels)) {
-                    if (h.distance - previousHotelDistance < 360 && h.distance - previousHotelDistance > 0
-                            && nextHotelDistance - h.distance < 360 && nextHotelDistance - h.distance > 0
-                            && currentHotel.rating < h.rating) {
-                        currentHotel = h;
-                        selectedHotels.set(i, currentHotel);
-                    }
-                }
-            }
-        }
+        for (Hotel h : selectedHotels)
+            System.out.println(h.distance + " " + h.rating);
+        System.out.println("------");
+        selectedHotels = optimizeRoute(totalTime, selectedHotels, hotels);
+
         averageRating = calculateAverageRating(selectedHotels);
-
-        while (selectedHotels.size() < 5) {
-            // if we still have stops left over use them to try and improve our average
-            // rating
-            averageRating = calculateAverageRating(selectedHotels);
-            Hotel bestRemaining = getBestRemainingHotel(hotels, selectedHotels);
-            if (bestRemaining.rating > averageRating)
-                selectedHotels.add(bestRemaining);
-            else
-                break;
-        }
-
-        Collections.sort(selectedHotels);
         for (Hotel h : selectedHotels)
             System.out.println(h.distance + " " + h.rating);
         System.out.println(averageRating);
@@ -189,6 +135,55 @@ public class App {
             Hotel currentHotel = getFarthestHotelWithinRange(hotels, currentTravelTime);
             selectedHotels.add(currentHotel);
             currentTravelTime = currentHotel.distance;
+        }
+        return selectedHotels;
+    }
+
+    static ArrayList<Hotel> optimizeRoute(int totalTime, ArrayList<Hotel> selectedHotels, ArrayList<Hotel> hotels) {
+        int previousHotelDistance = 0;
+        int nextHotelDistance = 0;
+        float averageRating;
+        ArrayList<Hotel> previousRoute = new ArrayList<Hotel>();
+
+        while (!previousRoute.equals(selectedHotels)) {
+            System.out.println("huh");
+            previousRoute = (ArrayList<Hotel>) selectedHotels.clone();
+            for (int i = selectedHotels.size() - 1; i > -1; i--) {
+                // adjust the hotels to try and optimize the average rating
+                if (i == 0) {
+                    previousHotelDistance = 0;
+                } else {
+                    previousHotelDistance = selectedHotels.get(i - 1).distance;
+                }
+                if (i == selectedHotels.size() - 1) {
+                    nextHotelDistance = totalTime;
+                } else {
+                    nextHotelDistance = selectedHotels.get(i + 1).distance;
+                }
+
+                Hotel currentHotel = selectedHotels.get(i);
+                for (Hotel h : getRemainingHotels(hotels, selectedHotels)) {
+                    if (h.distance - previousHotelDistance < 360 && h.distance - previousHotelDistance > 0
+                            && nextHotelDistance - h.distance < 360 && nextHotelDistance - h.distance > 0
+                            && currentHotel.rating < h.rating) {
+                        currentHotel = h;
+                        selectedHotels.set(i, currentHotel);
+                    }
+                }
+                averageRating = calculateAverageRating(selectedHotels);
+
+                while (selectedHotels.size() < 5) {
+                    // if we still have stops left over use them to try and improve our average
+                    // rating
+                    averageRating = calculateAverageRating(selectedHotels);
+                    Hotel bestRemaining = getBestRemainingHotel(hotels, selectedHotels);
+                    if (bestRemaining.rating > averageRating)
+                        selectedHotels.add(bestRemaining);
+                    else
+                        break;
+                }
+                Collections.sort(selectedHotels);
+            }
         }
         return selectedHotels;
     }
