113ea39a09d986c15aa3b8f36b01b39aab526ad5
Max <90890783+Maxgamz@users.noreply.github.com>
2021-10-05 12:31:04 +0200

    einlesen a5

--- a5/Marktwaage.java
+++ a5/Marktwaage.java
@@ -1,6 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Marktwaage{
-    
-    public Marktwaage(){
-        
+
+    ArrayList<String> lines;
+    String[] parts;
+    String gewichte;
+
+    public Marktwaage(String input){
+        gewichte = input;
+        einlesen(gewichte);
+    }
+
+    private void einlesen(String input){
+        File file = new File(input);
+        lines = new ArrayList<String>();
+        try{
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNext()){
+                parts = (scanner.nextLine()).split(" ");
+                lines.add(parts[0]);
+                if(parts.length == 2){
+                    lines.add(parts[1]);
+                }
+            }
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        //parts = lines.get(0).split(" ");
     }
 }
