68349c2332630e2dff503f332fc96169977dadf4
Manugamez <47077777+Manugamez@users.noreply.github.com>
2021-10-07 09:30:11 +0200

    Player class and test Data

--- a4/app/src/main/java/a4/App.java
+++ a4/app/src/main/java/a4/App.java
@@ -3,12 +3,59 @@
  */
 package a4;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class App {
+    static ArrayList<int[]> dice = new ArrayList<int[]>();
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        // to run with custom test Files replace yourPath with the path of that file:
+        // ./gradlew run --args="yourPath"
+        // Standard test file
+        File file = new File("src/main/resources/wuerfel0.txt");
+        
+        //reading out path from arguments
+        if (args.length == 1) {
+            try {
+                file = new File(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(file.getAbsolutePath());
+
+        ArrayList<String> lines = new ArrayList<String>();
+
+        try {
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+
+                String line = scanner.nextLine();
+
+                if (!line.startsWith("// ")) {
+                    lines.add(line);
+                }
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 1; i<lines.size(); i++){
+            String[] faces = lines.get(i).split(" ");
+            int[] die = new int[faces.length];
+            for (int j = 0; j<faces.length; j++){
+                die[j] = Integer.parseInt(faces[j]);
+            }
+            dice.add(die);
+        }
+
     }
 }
