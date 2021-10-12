48cc68da4ecd36434a22a02de39bbb985e052583
Nils <nils.staecker@gmail.com>
2021-10-12 13:08:02 +0200

    bisschen wie binär

--- a5/Marktwaage.java
+++ a5/Marktwaage.java
@@ -13,6 +13,10 @@ public class Marktwaage{
         einlesen(input);
     }
 
+    public Marktwaage(){
+        this("gewichtsstuecke0.txt");
+    }
+
     private void einlesen(String input){
         File file = new File(input);
         lines = new ArrayList<String>();
@@ -59,7 +63,7 @@ public class Marktwaage{
         return false;
     }
 
-    /*public void NilsMethode(){
+    public void NilsMethode(){
         for(int i=10;i<=10000;i+=10){
             if(noRekursion(i)){
                 System.out.println(i +" g: möglich");
@@ -68,45 +72,93 @@ public class Marktwaage{
             }
         }
     }
-    
-    private boolean noRekursion(int gewicht){
+
+    public int potenzieren(int basis, int expo){
+        int ergebnis=basis;
+        for(int i=1;i<expo;i++){
+            ergebnis*=ergebnis;
+        }
+        return ergebnis;
+    }
+
+    public boolean noRekursion(int gewicht){
         String binar="1";
         int gegengewicht;
+        int gewichtsadd;
         for(int i=1;i<lines.size();i++){
             binar+="0";
         }
-        for(int i=0;i<10000;i++){
-            gegengewicht=0;
-            char[] tf=binar.toCharArray();
-            for(int h=0;h<tf.length;h++){
-                if(tf[h]=='1'){
-                    gegengewicht+=Integer.parseInt(lines.get(h+1));
+        for(int i=0;i<3;i++){
+            for(int k=0;k<(lines.size()-1);k++){
+                gegengewicht=0;
+                gewichtsadd=0;
+                char[] tf=binar.toCharArray();
+                for(int h=0;h<(tf.length-1);h++){
+                    if(tf[h]=='1'){
+                        gegengewicht+=Integer.parseInt(lines.get(h+1));
+                    }
                 }
-            }
-            if(gegengewicht==gewicht){
-                return true;
-            }else{
-                binar=binarAddieren(binar);
+                for(int h=0;h<(tf.length-1);h++){
+                    if(tf[h]=='2'){
+                        gewichtsadd+=Integer.parseInt(lines.get(h+1));
+                    }
+                }
+                if(gegengewicht==(gewicht-gewichtsadd)){
+                    return true;
+                }else{
+                    binar=binarAddieren(binar,i);
+                }
             }
         }
         return false;
     }
 
-    private String binarAddieren(String binar){
+    public String binarAddieren(String binar, int durchgang){
         String ausgabe="";
         char[] ne=binar.toCharArray();
-        for(int i=0;i<ne.length;i++){
-            if(ne[i]==0){
-                ne[i]=1;
-                for(int h=i;h>=0;h--){
-                    ne[h]=0;
+
+        if(durchgang==0){
+            for(int i=0;i<ne.length;i++){
+                if(ne[i]=='0'){
+                    ne[i]='1';
+                    for(int h=(i-1);h>=0;h--){
+                        ne[h]='0';
+                    }
+                    break;
                 }
-                break;
             }
+            for(int i=0;i<ne.length;i++){
+                ausgabe+=ne[i];
+            }
+            return ausgabe;
+        } else if(durchgang==1){
+            for(int i=0;i<ne.length;i++){
+                if(ne[i]=='1'){
+                    ne[i]='2';
+                    for(int h=(i-1);h>=0;h--){
+                        ne[h]='1';
+                    }
+                    break;
+                }
+            }
+            for(int i=0;i<ne.length;i++){
+                ausgabe+=ne[i];
+            }
+            return ausgabe;
+        }else{
+            for(int i=(ne.length-1);i>=0;i--){
+                if(ne[i]=='2'){
+                    ne[i]='1';
+                    for(int h=(i+1);h<ne.length;h++){
+                        ne[h]='0';
+                    }
+                    break;
+                }
+            }
+            for(int i=0;i<ne.length;i++){
+                ausgabe+=ne[i];
+            }
+            return ausgabe;
         }
-        for(int i=0;i<ne.length;i++){
-            ausgabe+=ne[i];
-        }
-        return ausgabe;
-    }*/
+    }
 }
