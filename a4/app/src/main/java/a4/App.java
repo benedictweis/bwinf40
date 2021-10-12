801309bf7784a9234fcae76acb07a94206ca3289
Benegamz <benegamz@t-online.de>
2021-10-12 13:12:07 +0200

    fortschritte vs

--- a4/app/src/main/java/a4/App.java
+++ a4/app/src/main/java/a4/App.java
@@ -9,14 +9,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
+
     static ArrayList<int[]> dice = new ArrayList<int[]>();
+
     public static void main(String[] args) {
         // to run with custom test Files replace yourPath with the path of that file:
         // ./gradlew run --args="yourPath"
         // Standard test file
         File file = new File("src/main/resources/wuerfel0.txt");
-        
-        //reading out path from arguments
+
+        // reading out path from arguments
         if (args.length == 1) {
             try {
                 file = new File(args[0]);
@@ -48,14 +50,38 @@ public class App {
             e.printStackTrace();
         }
 
-        for (int i = 1; i<lines.size(); i++){
+        for (int i = 1; i < lines.size(); i++) {
+
             String[] faces = lines.get(i).split(" ");
-            int[] die = new int[faces.length];
-            for (int j = 0; j<faces.length; j++){
-                die[j] = Integer.parseInt(faces[j]);
+            int[] die = new int[faces.length-1];
+
+            for (int j = 1; j < faces.length; j++) {
+
+                die[j-1] = Integer.parseInt(faces[j]);
             }
+
             dice.add(die);
         }
+
+        for(int[] die: dice){
+            for (int i = 0; i < die.length; i++) {
+                System.out.print(die[i] +" ");
+            }
+            System.out.println();
+        }
+
+        int[] wins = new int[dice.size()-1];
+
+        for(int j = 0; j < dice.size(); j++) {
+            for (int i = j+1; i < dice.size(); i++) {
+                
+                System.out.print(j +" VS "+i+", ");
+                Player p1 = new Player();
+                System.out.println(" ");
+            }
+        }
+
+
 
     }
 }
