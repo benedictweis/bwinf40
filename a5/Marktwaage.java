186f00a94d806917628c4cec1ccab764dc292f5e
Max <90890783+Maxgamz@users.noreply.github.com>
2021-10-12 12:00:18 +0200

    a5

--- a5/Marktwaage.java
+++ a5/Marktwaage.java
@@ -44,15 +44,20 @@ public class Marktwaage{
     }
 
     public boolean istMöglich(int gewicht){
-        for(int i = 1; i < lines.size() - 1; i++){
-            if(gewicht == Integer.parseInt(lines.get(i))){
+        int startgewicht = gewicht;
+
+        for(int i = 1; i < lines.size(); i++){
+            if(startgewicht == Integer.parseInt(lines.get(i))){
                 return true;
             }
         }
 
-        for(int i = lines.size(); i < 1; i--){
-            if((gewicht - Integer.parseInt(lines.get(i))) > 0){
+        for(int i = lines.size() - 1; i > 0; i--){
+            if((gewicht - Integer.parseInt(lines.get(i))) >= 0){
                 gewicht -= Integer.parseInt(lines.get(i));
+                if(gewicht == 0){
+                    return true;
+                }
             }
         }
 
@@ -60,53 +65,53 @@ public class Marktwaage{
     }
 
     /*public void NilsMethode(){
-        for(int i=10;i<=10000;i+=10){
-            if(noRekursion(i)){
-                System.out.println(i +" g: möglich");
-            }else{
-                System.out.println(i +" g: nicht möglich");
-            }
-        }
+    for(int i=10;i<=10000;i+=10){
+    if(noRekursion(i)){
+    System.out.println(i +" g: möglich");
+    }else{
+    System.out.println(i +" g: nicht möglich");
     }
-    
+    }
+    }
+
     private boolean noRekursion(int gewicht){
-        String binar="1";
-        int gegengewicht;
-        for(int i=1;i<lines.size();i++){
-            binar+="0";
-        }
-        for(int i=0;i<10000;i++){
-            gegengewicht=0;
-            char[] tf=binar.toCharArray();
-            for(int h=0;h<tf.length;h++){
-                if(tf[h]=='1'){
-                    gegengewicht+=Integer.parseInt(lines.get(h+1));
-                }
-            }
-            if(gegengewicht==gewicht){
-                return true;
-            }else{
-                binar=binarAddieren(binar);
-            }
-        }
-        return false;
+    String binar="1";
+    int gegengewicht;
+    for(int i=1;i<lines.size();i++){
+    binar+="0";
+    }
+    for(int i=0;i<10000;i++){
+    gegengewicht=0;
+    char[] tf=binar.toCharArray();
+    for(int h=0;h<tf.length;h++){
+    if(tf[h]=='1'){
+    gegengewicht+=Integer.parseInt(lines.get(h+1));
+    }
+    }
+    if(gegengewicht==gewicht){
+    return true;
+    }else{
+    binar=binarAddieren(binar);
+    }
+    }
+    return false;
     }
 
     private String binarAddieren(String binar){
-        String ausgabe="";
-        char[] ne=binar.toCharArray();
-        for(int i=0;i<ne.length;i++){
-            if(ne[i]==0){
-                ne[i]=1;
-                for(int h=i;h>=0;h--){
-                    ne[h]=0;
-                }
-                break;
-            }
-        }
-        for(int i=0;i<ne.length;i++){
-            ausgabe+=ne[i];
-        }
-        return ausgabe;
+    String ausgabe="";
+    char[] ne=binar.toCharArray();
+    for(int i=0;i<ne.length;i++){
+    if(ne[i]==0){
+    ne[i]=1;
+    for(int h=i;h>=0;h--){
+    ne[h]=0;
+    }
+    break;
+    }
+    }
+    for(int i=0;i<ne.length;i++){
+    ausgabe+=ne[i];
+    }
+    return ausgabe;
     }*/
 }
