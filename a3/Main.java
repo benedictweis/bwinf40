369c75a64023471ef6dde8c85370f6db1bb9be0c
Max <90890783+Maxgamz@users.noreply.github.com>
2021-11-18 09:27:35 +0100

    a3 funktioniert wieder

--- a3/Main.java
+++ a3/Main.java
@@ -2,16 +2,18 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
             new Wortsuche("beispiele/worte1.txt").level1();
+            return;
         }
 
-        if(args[0] == "all"){
-            for(int i = 0; i < 5; i++){
+        if(args[0].equalsIgnoreCase("all")){
+            for(int i = 1; i < 6; i++){
                     new Wortsuche("beispiele/worte" + i + ".txt").level1();
                     new Wortsuche("beispiele/worte" + i + ".txt").level2();
                     new Wortsuche("beispiele/worte" + i + ".txt").level3();
             }
+            return;
         }
-        
+
         if (args[1].equals("1")) {
             new Wortsuche(args[0]).level1();
         } else if (args[1].equals("2")) {
